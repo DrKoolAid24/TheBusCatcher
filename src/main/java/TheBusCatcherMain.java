b9d7442a8545e44750e6a8e18faf2d4a5f48c4b3
DrKoolAid24 <fantomfoto@gmail.com>
2017-02-04 15:37:39 -0700

    test

---
+++ src/main/java/TheBusCatcherMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author skilker
+ *
+ */
+public class TheBusCatcherMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

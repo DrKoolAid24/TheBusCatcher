afcfaf8edb8aab32f0f292f76860223f471d7082
DrKoolAid24 <socks999@msn.com>
2017-02-05 11:04:52 -0700

    Added first JUnit test

---
+++ src/test/java/TheBusCatcherMainTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author skilker
+ *
+ */
+public class TheBusCatcherMainTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link TheBusCatcherMain#main(java.lang.String[])}.
+	 */
+	@Test
+	public final void testMain() {
+		fail("Not yet implemented");
+		assert(true);
+	}
+
+}

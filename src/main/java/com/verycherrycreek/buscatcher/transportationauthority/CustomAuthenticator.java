0f33933e62afdfebe171812904de53556b3645e7
DrKoolAid24 <socks999@msn.com>
2017-02-18 12:59:47 -0700

    SQ - created private members to set during authentication call

--- src/main/java/com/verycherrycreek/buscatcher/transportationauthority/CustomAuthenticator.java
+++ src/main/java/com/verycherrycreek/buscatcher/transportationauthority/CustomAuthenticator.java
@@ -12,6 +12,10 @@ import java.net.PasswordAuthentication;
 public class CustomAuthenticator extends Authenticator {
 	private String username;
 	private String password;
+	private String prompt;
+	private String hostname;
+	private InetAddress ipaddr;
+	private int port;
 	
 	public CustomAuthenticator(String pUsername, String pPassword) {
 		username = pUsername;
@@ -21,10 +25,10 @@ public class CustomAuthenticator extends Authenticator {
 	protected PasswordAuthentication getPasswordAuthentication() {
 		
 		// Get information about the request
-		String prompt = getRequestingPrompt();
-		String hostname = getRequestingHost();
-		InetAddress ipaddr = getRequestingSite();
-		int port = getRequestingPort();
+		prompt = getRequestingPrompt();
+		hostname = getRequestingHost();
+		ipaddr = getRequestingSite();
+		port = getRequestingPort();
 
 		// Return the information (a data holder that is used by Authenticator)
 		return new PasswordAuthentication(username, password.toCharArray());

ad0ec0c7ce87bba2aae573f9f9db2518178c460b
DrKoolAid24 <socks999@msn.com>
2017-02-20 07:52:32 -0700

    Added Converter package

---
+++ src/main/java/com/verycherrycreek/buscatcher/converter/Converter.java
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------
+ *
+ * Author: Scott Kilker        
+ *
+ *-------------------------------------------------------------------------*/
+package com.verycherrycreek.buscatcher.converter;
+
+/**
+ * @author skilker
+ *
+ */
+public abstract class Converter {
+
+}
---
+++ src/main/java/com/verycherrycreek/buscatcher/converter/RTDtoMongoDBConverter.java
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------
+ *
+ * Author: Scott Kilker        
+ *
+ *-------------------------------------------------------------------------*/
+package com.verycherrycreek.buscatcher.converter;
+
+/**
+ * @author skilker
+ *
+ */
+public class RTDtoMongoDBConverter extends Converter implements ConverterI {
+
+}
